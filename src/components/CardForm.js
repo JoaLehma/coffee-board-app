5cf17bbb8595c34af323e6adfd059c6121ab9d9d
Joachim Lehmann <joa.lehmann@web.de>
2022-08-05 16:37:44 +0200

    Add CardForm

---
+++ src/components/CardForm.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function CardForm({ categories, product, onSubmit }) {
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    onSubmit({
+      author,
+      content,
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Author{" "}
+          <input
+            name="author"
+            value={author}
+            onChange={(event) => {
+              setAuthor(event.target.value);
+            }}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Content{" "}
+          <input
+            name="content"
+            value={content}
+            onChange={(event) => {
+              setContent(event.target.value);
+            }}
+          />
+        </label>
+      </div>
+      <button type="submit">submit</button>
+    </form>
+  );
+}
