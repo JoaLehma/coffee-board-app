574bc3de8f6c248777991a408bffda4c5227f369
Joachim Lehmann <joa.lehmann@web.de>
2022-08-05 14:13:09 +0200

    Add cardModel File

---
+++ src/models/cardModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const cardSchema = new Schema({
+  author: { type: String, required: true },
+  content: { type: String, required: true },
+});
+
+const Card = mongoose.models.Category || mongoose.model("Card", cardSchema);
+
+export default Card;
